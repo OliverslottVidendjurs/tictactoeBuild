a2eaa783c7c85003964dc90e7918cd36f377ccaf
OliverslottVidendjurs <oliv169n@videndjurs.net>
2019-09-27 12:37:51 +0200

    first commit

---
+++ precache-manifest.1b473cded1349ae156bf2de689ff0f53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a989145fc1671a729fc7f777038beae7",
+    "url": "/tictactoeBuild/index.html"
+  },
+  {
+    "revision": "a9ecdb2ce29cdea9628a",
+    "url": "/tictactoeBuild/static/css/main.2a34271c.chunk.css"
+  },
+  {
+    "revision": "3350713b653208276ac1",
+    "url": "/tictactoeBuild/static/js/2.41278c5e.chunk.js"
+  },
+  {
+    "revision": "a9ecdb2ce29cdea9628a",
+    "url": "/tictactoeBuild/static/js/main.cc242ae3.chunk.js"
+  },
+  {
+    "revision": "fdd3c83a9ff8e4d79305",
+    "url": "/tictactoeBuild/static/js/runtime-main.4a0f5d83.js"
+  }
+]);
